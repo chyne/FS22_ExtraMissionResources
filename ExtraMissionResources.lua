541ee165d9765af64a6299c5a61c6af549e22af2
Christopher Hyne <christopher.hyne@gmail.com>
2022-02-02 10:58:16 -0500

    Refactor

--- ExtraMissionResources.lua
+++ ExtraMissionResources.lua
@@ -5,7 +5,9 @@ function ExtraMissionResources:loadMap(name)
 end
 
 function ExtraMissionResources:getIsMissionWorkAllowed(parentFunc, farmId, x, z, workAreaType)
-    return parentFunc(self, farmId, x, z, workAreaType) or (parentFunc(self, farmId, x, z, WorkAreaType.COMBINESWATH) and (workAreaType == WorkAreaType.FORAGEWAGON or workAreaType == WorkAreaType.BALER)) or (parentFunc(self, farmId, x, z, WorkAreaType.CULTIVATOR) and workAreaType == WorkAreaType.STONEPICKER) or (parentFunc(self, farmId, x, z, WorkAreaType.PLOW) and workAreaType == WorkAreaType.STONEPICKER)
+    return parentFunc(self, farmId, x, z, workAreaType) or 
+        (parentFunc(self, farmId, x, z, WorkAreaType.COMBINESWATH) and (workAreaType == WorkAreaType.FORAGEWAGON or workAreaType == WorkAreaType.BALER)) or 
+        ((parentFunc(self, farmId, x, z, WorkAreaType.CULTIVATOR) or parentFunc(self, farmId, x, z, WorkAreaType.PLOW)) and workAreaType == WorkAreaType.STONEPICKER)
 end
 
 addModEventListener(ExtraMissionResources)
